90199a3f84cf79b8c69c8ddeb1daff086b475350
Brian Kariuki <kariukibrian087@gmail.com>
2023-09-30 00:58:04 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion(char *s)-prints the strings
+ * @s:the string
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+{
+_putchar('\n');
+}
+}
