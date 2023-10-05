95006ef07a14443445dc52a563ca3d8629b9c670
Brian Kariuki <kariukibrian087@gmail.com>
2023-10-05 10:53:23 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked(unsigned int b)-chekes memory allocation
+ * @b:the size
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+char *ptr = malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
+}
